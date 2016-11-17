d2bf4f31a7132a7f7c6c3e08fc097b5b0886b6bf
Justin Suen <jsuen27@gmail.com>
2016-11-17 15:46:11 -0800

    Created populate_grid and sample_bomb

--- board.rb
+++ board.rb
@@ -2,7 +2,7 @@ class Board
 
   attr_accessor :grid, :size
 
-  def self.empty_grid(size)
+  def self.empty_grid
     Array.new(size) do
       Array.new(size) { Tile.new }
     end
@@ -10,6 +10,20 @@ class Board
 
   def initialize(size = 9)
     @size = size
-    @grid = self.empty_grid(size)
+    @grid = self.empty_grid
   end
+
+  def populate_grid
+    bomb_indices = sample_bomb(size)
+    bomb_indices.each do |index|
+      row = index / size
+      col = index % size
+      @grid[row][col].bomb = true
+    end
+  end
+
+  def sample_bomb(num_bombs)
+    (0...(num_bombs**2)).to_a.sample(num_bombs)
+  end
+
 end
