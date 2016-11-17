28a46fd3b6c92d3aed77cd592dfa809f9ef7d501
Justin Suen <jsuen27@gmail.com>
2016-11-17 15:19:25 -0800

    Initialized empty_grid method

--- board.rb
+++ board.rb
@@ -1,11 +1,15 @@
 class Board
 
-  attr_accessor :grid
+  attr_accessor :grid, :size
 
-  def self.from_file
+  def self.empty_grid(size)
+    Array.new(size) do
+      Array.new(size) { Tile.new }
+    end
   end
 
-  def initialize
-    @grid = grid
+  def initialize(size = 9)
+    @size = size
+    @grid = self.empty_grid(size)
   end
 end
