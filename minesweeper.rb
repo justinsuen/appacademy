961b3fa7e219052eadfc111e5ced5a55a80584de
Justin Suen <jsuen27@gmail.com>
2016-11-17 16:22:25 -0800

    Play game methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,15 @@
 class MinesweeperGame
-  
+  attr_reader :board
+
+  def initialize(size = 9)
+    @board = Board.new(size)
+  end
+
+  def play
+    board.populate_grid
+    board.render
+  end
 end
+
+game = MinesweeperGame.new(9)
+game.play
