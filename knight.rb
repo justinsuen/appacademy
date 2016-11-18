2dc441e26800a000146dbc21a4f95163f85c7bf1
Justin Suen <jsuen27@gmail.com>
2016-11-18 12:08:39 -0800

    Phase II completed: possible bugs (many)

--- knight.rb
+++ knight.rb
@@ -3,6 +3,9 @@ require_relative "polytreenode"
 class KnightPathFinder
   DELTAS = [[-2, 1], [-2, 1], [-1, -2], [-1, 2], [2, 1], [2, -1],
             [1, 2], [1, -2]]
+
+  attr_reader :curr_pos, :visited_positions, :moves
+
   def initialize(start_pos)
     @curr_pos = start_pos
     @root = PolyTreeNode.new(start_pos)
@@ -11,12 +14,29 @@ class KnightPathFinder
   end
 
   def build_move_tree
+    new_moves = new_move_positions(@curr_pos)
+    new_moves.each do |move|
+      @moves[move] = @root
+    end
 
+    until new_moves.empty?
+      current_move = PolyTreeNode.new(new_moves.shift)
+      current_move.parent = @moves[current_move.value]
+      next_moves = new_move_positions(current_move.value)
+
+      next_moves.each { |next_move| @moves[next_move] = current_move }
+
+      new_moves.concat(next_moves)
+    end
+
+    @root
   end
 
   def new_move_positions(pos)
     new_moves = valid_moves(pos).reject { |el| @visited_positions.include?(el) }
-    @visited_positions.concat(new_moves)
+    new_moves.each do |el|
+      @visited_positions << el unless @visited_positions.include?(el)
+    end
     new_moves
   end
 
@@ -24,7 +44,7 @@ class KnightPathFinder
     row, col = pos
     DELTAS.map do |delta|
       x, y = delta
-      if (row + x).between?(0, 8) && (col + y).between?(0, 8)
+      if (row + x).between?(0, 7) && (col + y).between?(0, 7)
         [x + row, col + y]
       end
     end.reject(&:nil?)
