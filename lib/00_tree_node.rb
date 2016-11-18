7a0e56582ae744086eadaf00293a7853b9ac7857
Justin Suen <jsuen27@gmail.com>
2016-11-18 10:48:55 -0800

    Basic polytreenode functionality

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent
+    @parent
+  end
+
+  def parent=(parent_node)
+    @parent.children.delete(self) if @parent
+    parent_node.children << self
+    @parent = parent_node
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
 
 end
