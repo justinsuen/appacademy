bf97217e5e18dc152454754e96f33e128037e766
Justin Suen <jsuen27@gmail.com>
2016-11-17 16:21:12 -0800

    Tile methods #bomb? #reveal? #to_s initialized

--- tile.rb
+++ tile.rb
@@ -1,9 +1,34 @@
+require 'colorize'
+
 class Tile
 
-  attr_reader :tile, :bomb
+  CHARS = {
+    :bomb       => "B",
+    :flag       => "F",
+    :unexplored => "*",
+    :empty      => "_"
+  }
+
+  DELTAS = [[-1, 1], [-1,0], [-1, -1], [0, -1],
+            [0, 1], [1, 1], [1, 0], [1, -1]]
+
+  attr_reader :tile, :bomb, :reveal
 
   def initialize(bomb = false)
     @bomb = bomb
+    @reveal = false
+  end
+
+  def bomb?
+    bomb
+  end
+
+  def reveal?
+    reveal
+  end
+
+  def to_s
+    char = bomb? ? "B".colorize(:red) : " "
   end
 
 end
