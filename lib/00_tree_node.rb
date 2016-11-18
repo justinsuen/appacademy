5eb686d08141d034751a63894453505d6a6fce6a
Justin Suen <jsuen27@gmail.com>
2016-11-18 11:08:23 -0800

    Finished bfs, dfs

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -11,8 +11,8 @@ class PolyTreeNode
 
   def parent=(parent_node)
     @parent.children.delete(self) if @parent
-    parent_node.children << self
     @parent = parent_node
+    parent_node.children << self unless parent_node.nil?
   end
 
   def children
@@ -21,6 +21,41 @@ class PolyTreeNode
 
   def value
     @value
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise error unless children.include?(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    children.each do |node|
+      current = node.dfs(target_value)
+      return current if current
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+
+    until queue.empty?
+      current = queue.shift
+      return current if current.value == target_value
+
+      current.children.each do |child|
+        queue << child
+      end
+    end
+
+    nil
   end
 
 end
