05abf6ea4c48acced2de3fc58857c70758c9036e
Justin Suen <jsuen27@gmail.com>
2016-11-17 17:42:48 -0800

    Started prompt methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -12,7 +12,30 @@ class MinesweeperGame
     @board.render
 
     until game_over?
+      com = get_command
+      pos = get_pos
+      do_command(com, pos)
+    end
+  end
 
+  def get_command
+    puts "what do you want to do? (f, r)"
+    gets.chomp
+  end
+
+  def get_pos
+    puts "input pos"
+    gets.chomp.split(",")
+  end
+
+  def do_command(com, pos)
+    case com
+    when "f"
+      board.flag(pos)
+    when "r"
+      board.reveal(pos)
+    else
+      puts "invalid command"
     end
   end
 
