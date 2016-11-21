b0b8edd81f45c9e6ea235bfbbeee53c693b893a0
Justin Suen <jsuen27@gmail.com>
2016-11-21 11:47:48 -0800

    Added tests

--- employee.rb
+++ employee.rb
@@ -55,6 +55,7 @@ class Manager < Employee
   end
 end
 
+# Tests
 ned = Manager.new("Ned", "Founder", 1000000)
 darren = Manager.new("Darren", "TA Manager", 78000, ned)
 shawna = Employee.new("Shawna", "TA", 12000, darren)
