9099ca8a7dd80978a5c51de1992acd43b08c2b30
Justin Suen <jsuen27@gmail.com>
2016-11-22 15:58:24 -0800

    Completed Pawn#valid_moves

--- chess/pieces/pawn.rb
+++ chess/pieces/pawn.rb
@@ -12,4 +12,38 @@ class Pawn < Piece
       " #{"\u2659".encode('utf-8')} "
     end
   end
+
+  def forward_dir
+    color == :white ? -1 : 1
+  end
+
+  def valid_moves
+    forward_moves.concat(attack_moves)
+  end
+
+  def forward_moves
+    curr_x, curr_y = pos
+    possible_moves = []
+
+    curr_x += forward_dir
+    poss_move = [curr_x, curr_y]
+
+    possible_moves << poss_move if board.in_bounds?(poss_move)
+
+    possible_moves
+  end
+
+  def attack_moves
+    curr_x, curr_y = pos
+    possible_moves = []
+
+    right = [curr_x + forward_dir, curr_y + 1]
+    left = [curr_x + forward_dir, curr_y -1]
+
+    [right, left].each do |move|
+      possible_moves << move if board[move].color != color
+    end
+
+    possible_moves
+  end
 end
