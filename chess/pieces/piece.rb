9ad3cf5da75d01e3c81919b8c912cbfb8c010b3f
Justin Suen <jsuen27@gmail.com>
2016-11-22 14:53:13 -0800

    Minor fixes

--- chess/pieces/piece.rb
+++ chess/pieces/piece.rb
@@ -1,29 +1,23 @@
 require "colorize"
 
 class Piece
-  RANKS = [:wK, :wQ, :wR, :wB, :wN, :wP, :bK, :bQ, :bR, :bB, :bN, :bP].freeze
-  RANK_CODES = ["\u2654", "\u2655", "\u2656", "\u2657", "\u2658", "\u2659",
-                "\u265A", "\u265B", "\u265C", "\u265D", "\u265E", "\u265F"].freeze
+  attr_reader :board, :color
+  attr_accessor :pos
 
-  attr_reader :rank
+  def initialize(board, color, pos)
+    @board, @color, @pos = board, color, pos
 
-  def initialize(rank)
-    @rank = rank
+    board.add_piece(self, pos)
   end
 
   def to_s
-    RANKS.each_with_index do |r, i|
-      return " #{RANK_CODES[i].encode('utf-8')} " if rank == r
+    symbol
+  end
+
+  def valid_moves
+    moves.select do |pos|
+      board.in_bounds?(pos) &&
+      (board.is_null?(pos) || board[pos].color != color)
     end
-  end
-end
-
-class NullPiece < Piece
-  def initialize(rank)
-    super(rank)
-  end
-
-  def to_s
-    "   "
   end
 end
