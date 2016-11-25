ec5d74eff33e2082cc41253de5f007d3cc7fde8c
Justin Suen <jsuen27@gmail.com>
2016-11-25 12:13:44 -0800

    complete p05 hash map with linked lists

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,18 +11,36 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+      resize! if @count >= num_buckets
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
   def each
+    @store.each do |linked_list|
+      linked_list.each do |link|
+        yield [link.key, link.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +61,17 @@ class HashMap
   end
 
   def resize!
+    arr = Array.new(num_buckets * 2) { LinkedList.new }
+    @store.each do |linked_list|
+      linked_list.each do |el|
+        arr[el.key.hash % (num_buckets * 2)].append(el.key, el.val)
+      end
+    end
+    @store = arr
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
