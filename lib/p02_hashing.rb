657b11f32f00c7bdc917fec1fbcec442b0776dd4
Justin Suen <jsuen27@gmail.com>
2016-11-25 10:54:47 -0800

    complete p02 - hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hash_val = 0
+    self.each_with_index do |num, i|
+      hash_val += num.hash * (i + 1)
+    end
+    hash_val
   end
 end
 
 class String
   def hash
+    self.split("").map(&:ord).hash
   end
 end
 
@@ -16,6 +22,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_val = 0
+    self.each do |k, v|
+      hash_val += k.hash + (v.hash * 2)
+    end
+    hash_val
   end
 end
