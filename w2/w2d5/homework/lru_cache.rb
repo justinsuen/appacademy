9c9e8097da2e826c9b9acec665340845bc86cb59
Justin Suen <jsuen27@gmail.com>
2016-11-25 16:04:39 -0800

    Started lru_cache.rb homework

---
+++ w2/w2d5/homework/lru_cache.rb
@@ -0,0 +1,40 @@
+class LRUCache
+  def initialize
+  end
+
+  def count
+    # returns number of elements currently in cache
+  end
+
+  def add(el)
+    # adds element to cache according to LRU principle
+  end
+
+  def show
+    # shows the items in the cache, with the LRU item first
+  end
+
+  private
+  # helper methods go here!
+
+end
+
+johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
