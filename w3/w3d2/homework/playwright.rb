cac002b3af64ec5ec8b6b40d78b6e07768da1e65
Justin Suen <jsuen27@gmail.com>
2016-11-28 17:02:40 -0800

    Initialized w3d2 plays.rb homework

---
+++ w3/w3d2/homework/playwright.rb
@@ -0,0 +1,22 @@
+require 'sqlite3'
+require_relative 'plays'
+
+class Playwright
+  def all
+  end
+
+  def find_by_name(name)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def get_plays
+  end
+end
