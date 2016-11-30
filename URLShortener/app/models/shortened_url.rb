f10861313d2873f65ebebddfae2c41756b112a01
Justin Suen <jsuen27@gmail.com>
2016-11-30 13:55:34 -0800

    Complete phase 2

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,4 +1,22 @@
 class ShortenedUrl < ActiveRecord::Base
-  validates :user_id, :short_url, presence: true, uniqueness: true
-  validates :long_url, presence: true
+  validates :user_id, :short_url, :long_url, presence: true
+  validates :short_url, uniqueness: true
+
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  def self.random_code
+    new_url = SecureRandom::urlsafe_base64
+    until !ShortenedUrl.exists?(short_url: new_url)
+      new_url = SecureRandom::urlsafe_base64
+    end
+    new_url
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(user_id: user.id, short_url: ShortenedUrl.random_code,
+                         long_url: long_url)
+  end
 end
--- URLShortener/app/models/user.rb
+++ URLShortener/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
+
+  has_many :submitted_urls,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :ShortenedUrl
 end
