1e641924d61b760a0f82c19bf6f54fd2c923550a
Justin Suen <jsuen27@gmail.com>
2016-12-02 10:24:20 -0800

    Complete 00_attr_accessor_object.rb

--- w3/w3d5/active_record_lite/lib/00_attr_accessor_object.rb
+++ w3/w3d5/active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) { instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |val| instance_variable_set("@#{name}", val) }
+    end
   end
 end
