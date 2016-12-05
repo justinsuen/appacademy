b95cf1c9a6e4c1c4b96f34a30b889491ffe8ba11
Justin Suen <jsuen27@gmail.com>
2016-12-05 13:52:14 -0800

    Basic routing requests

--- w4/w4d1/FirstRailsProject/app/controllers/users_controller.rb
+++ w4/w4d1/FirstRailsProject/app/controllers/users_controller.rb
@@ -2,4 +2,28 @@ class UsersController < ApplicationController
   def index
     render text: "I'm in the index action!"
   end
+
+  def create
+    render text: "I'm in the create action!"
+  end
+
+  def new
+    render text: "I'm in the new action!"
+  end
+
+  def edit
+    render text: "I'm in the edit action!"
+  end
+
+  def show
+    render text: "I'm in the show action!"
+  end
+
+  def update
+    render text: "I'm in the update action!"
+  end
+
+  def destroy
+    render text: "I'm in the destroy action!"
+  end
 end
