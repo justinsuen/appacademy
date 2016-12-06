edfd5ea1b2bacc3a5f188b62d3e2147ef54174b0
Justin Suen <jsuen27@gmail.com>
2016-12-06 13:40:33 -0800

    Complete #update

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,6 @@
 class CatsController < ApplicationController
   def index
-    @cats = Cat.all
+    @cats = Cat.all.order(:name)
     render :index
   end
 
@@ -31,6 +31,16 @@ class CatsController < ApplicationController
   def edit
     @cat = Cat.find_by(id: params[:id])
     render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
   end
 
   private
