3decef13a6e91299690f040904e7de89358513d8
Justin Suen <jsuen27@gmail.com>
2016-12-07 13:29:01 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 2.0'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '~>4.2.0'
+gem 'sass-rails', '~> 4.0.3'
+gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'spring'
+  gem 'pry-rails'
+end
---
+++ app/models/cat.rb
@@ -0,0 +1,28 @@
+require 'action_view'
+
+class Cat < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
+
+  CAT_COLORS = %w(black white orange brown)
+
+  has_many(
+    :rental_requests,
+    class_name: "CatRentalRequest",
+    dependent: :destroy
+  )
+
+  validates(
+    :birth_date,
+    :color,
+    :name,
+    :sex,
+    presence: true
+  )
+
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: %w(M F)
+
+  def age
+    time_ago_in_words(birth_date)
+  end
+end
