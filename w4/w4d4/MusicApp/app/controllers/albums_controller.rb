c734639495c65fe1c1102e77e2ae63c85091a4e1
Justin Suen <jsuen27@gmail.com>
2016-12-08 15:13:55 -0800

    Complete basic album controller and views

--- w4/w4d4/MusicApp/app/controllers/albums_controller.rb
+++ w4/w4d4/MusicApp/app/controllers/albums_controller.rb
@@ -19,9 +19,44 @@ class AlbumsController < ApplicationController
     end
   end
 
+  def edit
+    @album = Album.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @album = Album.find_by(id: params[:id])
+
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find_by(id: params[:id])
+    @band = Band.find_by(id: @album.band_id)
+
+    if @album
+      Album.destroy(params[:id])
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :show
+    end
+  end
+
   def show
     @album = Album.find_by(id: params[:id])
     @band = Band.find_by(id: @album.band_id)
     render :show
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :album_type, :band_id)
   end
 end
