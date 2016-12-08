864e42b6a7d3aa939e2cd153627e7053f9238237
Justin Suen <jsuen27@gmail.com>
2016-12-08 09:53:27 -0800

    Annotate user model and debug validations

--- w4/w4d4/MusicApp/app/models/user.rb
+++ w4/w4d4/MusicApp/app/models/user.rb
@@ -1,11 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
   include BCrypt
-
-  after_initialize :ensure_session_token
+  attr_reader :password
 
   validates :email, :password_digest, :session_token, presence: true
   validates :email, :session_token, uniqueness: true
-  validates :password_digest, allow_nil: true
+  validates :password_digest, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
@@ -17,13 +30,13 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64(128)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(password) == self.password_digest
+  end
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
-  end
-
-  def is_password?(password)
-    BCrypt::Password.new(password) == self.password_digest
   end
 
   def reset_session_token
