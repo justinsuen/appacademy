4c48ad928c48bfb8f2f697f792afa1e6656af6d1
Justin Suen <jsuen27@gmail.com>
2016-12-08 12:21:40 -0800

    Debug band views and controller

---
+++ w4/w4d4/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,55 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def create
+    @band = Band.new(params[:band][:name])
+
+    if @band.save
+      redirect_to bands_url
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @band = Band.find(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find(id: params[:id])
+
+    if @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @band = Band.find(id: params[:id])
+
+    if @band
+      Band.destroy(params[:id])
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
