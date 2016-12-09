ee404e0de990172dfff0f38297b502370e254c8a
Justin Suen <jsuen27@gmail.com>
2016-12-08 17:24:31 -0800

    Finish ugly_lyrics method

--- w4/w4d4/MusicApp/app/helpers/tracks_helper.rb
+++ w4/w4d4/MusicApp/app/helpers/tracks_helper.rb
@@ -1,2 +1,10 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    formatted_lyrics = ""
+    lyrics.lines.each do |line|
+      formatted_lyrics << "&#9835; #{h(line)}"
+    end
+
+    "<pre>#{formatted_lyrics}</pre>".html_safe
+  end
 end
