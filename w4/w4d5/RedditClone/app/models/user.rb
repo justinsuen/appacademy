9129b7ab636e24bcce290b767bc8245a99274646
Justin Suen <jsuen27@gmail.com>
2016-12-09 21:43:01 -0800

    Basic user views

--- w4/w4d5/RedditClone/app/models/user.rb
+++ w4/w4d5/RedditClone/app/models/user.rb
@@ -11,7 +11,6 @@
 #
 
 class User < ActiveRecord::Base
-  include BCrypt
   attr_reader :password
 
   validates :username, :password_digest, :session_token, presence: true, uniqueness: true
--- w4/w4d5/RedditClone/config/routes.rb
+++ w4/w4d5/RedditClone/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # root to: "/subs"
 
   resources :users, except: [:index, :destroy]
-  resource :session
+  resource :session, only: [:new, :create, :destroy]
 end
