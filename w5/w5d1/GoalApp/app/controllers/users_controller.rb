132bddf2eab278bf77e989a66b68280ce3e5a051
Justin Suen <jsuen27@gmail.com>
2016-12-12 11:47:53 -0800

    Complete user views tests

--- w5/w5d1/GoalApp/app/controllers/users_controller.rb
+++ w5/w5d1/GoalApp/app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+
+    if @user
+      render :show
+    else
+      flash[:errors] = "User does not exist"
+      render :index
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(username: user_params[:username], password: user_params[:password])
+
+    if @user.save
+      render :show
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- w5/w5d1/GoalApp/spec/controllers/users_controller_spec.rb
+++ w5/w5d1/GoalApp/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { User.create!(username: "kobe_bryant", password: "black_mamba") }
 
+  describe "GET #index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, link: {}
+      expect(response).to render_template(:new)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET #show" do
+    it "with invalid id - renders index template" do
+      get :show, id: 10000
+      expect(response).to render_template(:index)
+      expect(flash[:errors]).to be_present
+    end
+
+    it "with valid id - renders show template" do
+      get :show, id: user.id
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "POST #create" do
+    it "with invalid params - no password" do
+      post :create, user: { username: "lebron_james", password: "" }
+      expect(response).to render_template(:new)
+      expect(flash[:errors]).to be_present
+    end
+
+    it "with invalid params - short password < 6 characters" do
+      post :create, user: { username: "lebron_james", password: "king" }
+      expect(response).to render_template(:new)
+      expect(flash[:errors]).to be_present
+    end
+
+    it "with valid params - redirect to user page on success" do
+      post :create, user: { username: "lebron_james", password: "theking" }
+      expect(response).to render_template(:show)
+
+    end
+  end
 end
