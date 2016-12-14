99eb3cd5855a75895d8e242c374881390fd93ca0
Justin Suen <jsuen27@gmail.com>
2016-12-13 17:27:17 -0800

    Complete phase 5 Router class

--- w5/w5d2/RailsLite/lib/controller_base.rb
+++ w5/w5d2/RailsLite/lib/controller_base.rb
@@ -7,9 +7,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
-    @req = req
-    @res = res
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @params = req.params.merge(route_params)
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
@@ -60,5 +61,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    self.render(name) unless already_built_response?
   end
 end
--- w5/w5d2/RailsLite/lib/router.rb
+++ w5/w5d2/RailsLite/lib/router.rb
@@ -2,15 +2,24 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    @pattern =~ req.path && @http_method == req.request_method.downcase.to_sym
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+
+    @controller_class.new(req, res, route_params).invoke_action(@action_name)
   end
 end
 
@@ -18,27 +27,42 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    self.instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each { |route| return route if route.matches?(req) }
+    nil
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    route = match(req)
+
+    if route
+      route.run(req, res)
+    else
+      res.status = 404
+    end
   end
 end
