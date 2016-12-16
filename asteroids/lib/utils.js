1a361a60e54a678fe114df81fdfd3c4e31a69bfe
Justin Suen <jsuen27@gmail.com>
2016-12-16 14:22:00 -0800

    Complete utils.js

--- asteroids/lib/utils.js
+++ asteroids/lib/utils.js
@@ -1,3 +1,11 @@
-function Util () {}
+const Util = {
+  inherits (childClass, parentClass) {
+    function Surrogate() {}
+
+    Surrogate.prototype = parentClass.prototype;
+    childClass.prototype = new Surrogate();
+    childClass.prototype.constructor = childClass;
+  }
+};
 
 module.export = Util;
