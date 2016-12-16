31d3dd79c4efbd6e413c5c2f96388ca094c920da
Justin Suen <jsuen27@gmail.com>
2016-12-16 11:41:58 -0800

    Complete currying

--- arguments.js
+++ arguments.js
@@ -12,6 +12,17 @@ function sum() {
 // console.log(sum(1, 2, 3, 4) === 10);
 // console.log(sum(1, 2, 3, 4, 5) === 15);
 
+function product() {
+  let accum = 1;
+  let args = Array.prototype.slice.call(arguments);
+
+  for (let i = 0; i < args.length; i++) {
+    accum *= args[i];
+  }
+
+  return accum;
+}
+
 function sum2(...args) {
   let accum = 0;
 
@@ -59,11 +70,55 @@ const markov = new Cat("Markov");
 const breakfast = new Cat("Breakfast");
 
 
-markov.says("meow", "Ned");
-// Markov says meow to Ned!
-markov.says.myBind2(breakfast, "meow", "Kush")();
-// Breakfast says meow to Kush!
-markov.says.myBind2(breakfast)("meow", "a tree");
-// Breakfast says meow to a tree!
-markov.says.myBind(breakfast, "meow")("Markov");
-// Breakfast says meow to Markov!
+// // Markov says meow to Ned!
+// markov.says("meow", "Ned");
+//
+// // Breakfast says meow to Kush!
+// markov.says.myBind2(breakfast, "meow", "Kush")();
+//
+// // Breakfast says meow to a tree!
+// markov.says.myBind2(breakfast)("meow", "a tree");
+//
+// // Breakfast says meow to Markov!
+// markov.says.myBind(breakfast, "meow")("Markov");
+
+
+function curriedSum (numArgs) {
+  let numbers = [];
+
+  function _curriedSum(n) {
+    numbers.push(n);
+
+    if (numbers.length === numArgs) {
+      return sum(...numbers);
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+//
+// const s = curriedSum(4);
+// console.log(s(5)(30)(20)(1)); // => 56
+
+
+Function.prototype.curry = function (numArgs) {
+  let args = [];
+  let that = this;
+
+  function _curried(n) {
+    args.push(n);
+
+    if (args.length === numArgs) {
+      return that(...args);
+    } else {
+      return _curried;
+    }
+  }
+
+  return _curried;
+};
+
+const s = product.curry(4);
+console.log(s(3)(3)(3)(3)); // => 81
