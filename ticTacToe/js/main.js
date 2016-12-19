a1106c6540693bc331c3cea28fa356784ec46ca5
Justin Suen <jsuen27@gmail.com>
2016-12-19 11:16:51 -0800

    Add basic board and display

--- ticTacToe/js/main.js
+++ ticTacToe/js/main.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("./game.js");
 
 $( () => {
   // Your code here
+  let $board = $(".ttt");
+  let game = new Game();
+  console.log(game);
+  let view = new View(game, $board);
+  console.log(view);
 });
--- ticTacToe/js/ttt-view.js
+++ ticTacToe/js/ttt-view.js
@@ -1,11 +1,21 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    $el.append(this.setupBoard());
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $ul = $('<ul></ul>');
+    for(let i = 0; i < 9; i++) {
+      $ul.append('<li></li>');
+    }
+    return $ul;
+  }
 }
 
 module.exports = View;
