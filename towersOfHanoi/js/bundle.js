719fd7b1cf0d36e305c0df6abf93687768d3e090
Justin Suen <jsuen27@gmail.com>
2016-12-19 15:21:37 -0800

    makeMove complete

--- towersOfHanoi/js/bundle.js
+++ towersOfHanoi/js/bundle.js
@@ -138,30 +138,47 @@
 	    this.$el = $el;
 
 	    this.setupBoard();
-	    // this.bindEvents();
+	    this.bindEvents();
 	  }
 
 	  bindEvents() {
-	    this.$el.on('click', 'li',  event => {
-	      const square = event.currentTarget;
-	      const $square = $(square);
-	      this.makeMove($square);
+	    this.$el.on('click', 'ul',  event => {
+	      const $tower = $(event.currentTarget);
+	      if ($('.selected').length === 1) {
+	        this.makeMove($('.selected'), $tower);
+	      } else {
+	        $tower.addClass('selected');
+	      }
 	    });
 	  }
 
-	  makeMove($square) {
-	    let pos = $square.data('pos');
+	  makeMove($startTower, $endTower) {
+	    let res = this.game.move($startTower.data('pos'), $endTower.data('pos'));
 
+	    if (res === true) {
+	      let disc = $('.selected .disc')[0];
+	      let $disc = $(disc);
+	      let classes = $disc.attr("class");
 
+	      $endTower.children().not('.disc').last().addClass(classes);
+	      $startTower.removeClass('selected');
+	      $disc.removeClass(classes);
+	    } else {
+	      $('.selected').removeClass('selected');
+	      console.log('Invalid move');
+	    }
 	  }
 
 	  setupBoard() {
 	    let $uls = $(".hanoi");
 	    for(let i = 0; i < 3; i++) {
 	      let $ul = $('<ul></ul>');
+	      $ul.data('pos', i);
 	      for(let j = 0; j < 3; j++) {
 	        let $li = $('<li></li>');
-	        $li.data('pos', [i, j]);
+	        if (i === 0) {
+	          $li.addClass('disc').addClass(`disc${j + 1}`);
+	        }
 	        $ul.append($li);
 	      }
 	      $uls.append($ul);
