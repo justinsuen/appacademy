826a2894e5d760c0b6eae6c672041f5e47668ed5
Justin Suen <jsuen27@gmail.com>
2016-12-22 16:37:47 -0800

    Used solutions for phase 4

--- w6/w6d4/jQueryLite/lib/jquery_lite.js
+++ w6/w6d4/jQueryLite/lib/jquery_lite.js
@@ -61,8 +61,58 @@
 	  }
 	};
 
+	window.$l.extend = (base, ...otherObjs) => {
+	  otherObjs.forEach(obj => {
+	    for (let prop in obj) {
+	      base[prop] = obj[prop];
+	    }
+	  });
+	  return base;
+	};
+
+	window.$l.ajax = options => {
+	  const request = new XMLHttpRequest();
+	  const defaults = {
+	    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+	    method: "GET",
+	    url: "",
+	    success: () => {},
+	    error: () => {},
+	    data: {}
+	  };
+
+	  options = window.$l.extend(defaults, options);
+	  options.method = options.method.toUpperCase();
+
+	  if (options.method === "GET") {
+	    options.url += "?" + toQueryString(options.data);
+	  }
+
+	  request.open(options.method, options.url, true);
+	  request.onload = e => {
+	    if (request.status === 200) {
+	      options.success(request.response);
+	    } else {
+	      options.error(request.response);
+	    }
+	  };
+
+	  request.send(JSON.stringify(options.data));
+	};
+
+	//helper methods
+	const toQueryString = obj => {
+	  let result = "";
+	  for (let prop in obj) {
+	    if (obj.hasOwnProperty(prop)) {
+	      result += prop + "=" + obj[prop] + "&";
+	    }
+	  }
+	  return result.substring(0, result.length - 1);
+	};
+
 	const registerDocReadyCallback = func => {
-	  if(!_docReady){
+	  if (!_docReady) {
 	    _docReadyCallbacks.push(func);
 	  } else {
 	    func();
@@ -71,7 +121,7 @@
 
 	document.addEventListener('DOMContentLoaded', () => {
 	  _docReady = true;
-	  _docReadyCallbacks.forEach( func => func() );
+	  _docReadyCallbacks.forEach(func => func());
 	});
 
 
