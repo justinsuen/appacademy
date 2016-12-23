1ec0f667ef313407479623db63fd18e0b02ccbff
Justin Suen <jsuen27@gmail.com>
2016-12-22 16:28:19 -0800

    Complete phase 3 - document ready callbacks

--- w6/w6d4/jQueryLite/lib/jquery_lite.js
+++ w6/w6d4/jQueryLite/lib/jquery_lite.js
@@ -46,17 +46,33 @@
 
 	const DOMNodeCollection = __webpack_require__(2);
 
+	const _docReadyCallbacks = [];
+	let _docReady = false;
+
 	window.$l = arg => {
 	  let argsList;
 	  if (arg instanceof HTMLElement) {
-	    argsList = [arg];
+	    return new DOMNodeCollection([arg]);
+	  } else if (typeof arg === 'function') {
+	    return registerDocReadyCallback(arg);
 	  } else {
 	    let nodeList = document.querySelectorAll(arg);
-	    argsList = Array.from(nodeList);
+	    return new DOMNodeCollection(Array.from(nodeList));
 	  }
-
-	  return new DOMNodeCollection(argsList);
 	};
+
+	const registerDocReadyCallback = func => {
+	  if(!_docReady){
+	    _docReadyCallbacks.push(func);
+	  } else {
+	    func();
+	  }
+	};
+
+	document.addEventListener('DOMContentLoaded', () => {
+	  _docReady = true;
+	  _docReadyCallbacks.forEach( func => func() );
+	});
 
 
 /***/ },
--- w6/w6d4/jQueryLite/lib/main.js
+++ w6/w6d4/jQueryLite/lib/main.js
@@ -1,13 +1,29 @@
 const DOMNodeCollection = require("./dom_node_collection.js");
 
+const _docReadyCallbacks = [];
+let _docReady = false;
+
 window.$l = arg => {
   let argsList;
   if (arg instanceof HTMLElement) {
-    argsList = [arg];
+    return new DOMNodeCollection([arg]);
+  } else if (typeof arg === 'function') {
+    return registerDocReadyCallback(arg);
   } else {
     let nodeList = document.querySelectorAll(arg);
-    argsList = Array.from(nodeList);
+    return new DOMNodeCollection(Array.from(nodeList));
   }
-
-  return new DOMNodeCollection(argsList);
 };
+
+const registerDocReadyCallback = func => {
+  if(!_docReady){
+    _docReadyCallbacks.push(func);
+  } else {
+    func();
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  _docReady = true;
+  _docReadyCallbacks.forEach( func => func() );
+});
