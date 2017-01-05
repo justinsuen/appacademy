e79514875f3b65b495fb0aff611792cbc10a85e6
Justin Suen <jsuen27@gmail.com>
2017-01-04 16:40:06 -0800

    Complete phase 2

--- w7/w7d3/pokedex/frontend/pokedex.jsx
+++ w7/w7d3/pokedex/frontend/pokedex.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
+import { selectAllPokemon } from './reducers/selectors';
 
 // Testing
 window.receiveAllPokemon = receiveAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
 window.requestAllPokemon = requestAllPokemon;
-
+window.selectAllPokemon = selectAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
