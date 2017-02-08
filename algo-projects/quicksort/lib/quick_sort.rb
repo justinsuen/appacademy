2e1800a36b3effc9c32d8aab71bd9cf0a3a70f76
Justin Suen <jsuen27@gmail.com>
2017-02-07 23:34:24 -0800

    Quick sort project

--- algo-projects/quicksort/lib/quick_sort.rb
+++ algo-projects/quicksort/lib/quick_sort.rb
@@ -4,12 +4,47 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.empty?
+
+    pivot = array[0]
+
+    left = array.select { |el| pivot > el }
+    middle = array.select { |el| pivot == el }
+    right = array.select { |el| pivot < el }
+
+    sort1(left) + middle + sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new { |i, j| i <=> j }
+
+    return array if length < 2
+
+    pivot_idx = partition(array, start, length, &prc)
+
+    left_length = pivot_idx - start
+    right_length = length - (left_length + 1)
+    sort2!(array, start, left_length, &prc)
+    sort2!(array, pivot_idx + 1, right_length, &prc)
+
+    array
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |i, j| i <=> j }
+
+    pivot_idx = start
+    pivot = array[start]
+
+    ((start + 1)...(start + length)).each do |idx|
+      if prc.call(pivot, array[idx]) > 0
+        array[idx], array[pivot_idx + 1] = array[pivot_idx + 1], array[idx]
+        pivot_idx += 1
+      end
+    end
+    array[start], array[pivot_idx] = array[pivot_idx], array[start]
+
+    pivot_idx
   end
 end
